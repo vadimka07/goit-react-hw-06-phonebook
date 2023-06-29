129974dbce043d965112e683e33582e5235468e5
Vadim <vadger@yahoo.com>
2023-06-29 11:09:06 +0300

    fix

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,16 +1,15 @@
 import {filterContact} from "../../redux/filterSlice";
 import {useDispatch, useSelector} from "react-redux";
-import {getContacts, getFilter} from "../../redux/selectors";
+import {getFilter} from "../../redux/selectors";
 
 function Filter() {
 
     const filterDispatch = useDispatch();
     const dataFilterSelector = useSelector(getFilter);
-    const contacts = useSelector(getContacts);
     const handlerFilter = e => {
         filterDispatch(filterContact(e.target.value))
     };
-    if(contacts.length < 2) return
+
     return (
         <input type="text" name="filter" value={dataFilterSelector} onChange={handlerFilter} />
     );
